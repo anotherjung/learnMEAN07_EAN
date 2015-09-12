abc8030fd18e717ce4f89dc9f57ef336c5aea7f3
Jung No <jungno@gmail.com>
2015-09-12 16:09:32 -0700

    cleaned up

--- server.js
+++ server.js
@@ -14,9 +14,6 @@ var mongoose = require('mongoose');
 app.use(bodyParser.urlencoded());
 //static content
 app.use(express.static(path.join(__dirname, "./client/static")));
-//setting up ejs and our views folder
-app.set('views', path.join(__dirname, './client/views'));
-app.set('view engine', 'ejs');
 
 	//start MVC1b require for routes
 	var routes_setter = require('./server/config/routes.js');
